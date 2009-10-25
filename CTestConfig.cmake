c228d6718f7a4de3d02d1774b6d307550b2d7f1b
dirvine <dirvine@6165b5c9-d236-0410-b717-85aa93a55b26>
2009-10-25 13:47:53 +0000

    small updates to drop site and IOU management
    git-svn-id: svn+ssh://dash.maidsafe.net/srv/svn/dawn/trunk@3621 6165b5c9-d236-0410-b717-85aa93a55b26

--- CTestConfig.cmake
+++ CTestConfig.cmake
@@ -8,6 +8,6 @@ set(CTEST_PROJECT_NAME "PerpetualData")
 set(CTEST_NIGHTLY_START_TIME "00:01:00 GMT")
 
 set(CTEST_DROP_METHOD "http")
-set(CTEST_DROP_SITE "dash2.maidsafe.net")
+set(CTEST_DROP_SITE "dash.maidsafe.net")
 set(CTEST_DROP_LOCATION "/submit.php?project=PerpetualData")
 set(CTEST_DROP_SITE_CDASH TRUE)
