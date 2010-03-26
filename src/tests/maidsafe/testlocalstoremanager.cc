c7989f5199336ad1ca58446775c7759664ea1097
Juan Smer <Smer@smer.office.maidsafe>
2010-03-26 12:10:17 +0000

    fixing segfaul in testlocalstoremanager

--- src/tests/maidsafe/testlocalstoremanager.cc
+++ src/tests/maidsafe/testlocalstoremanager.cc
@@ -97,7 +97,7 @@ void PacketOpCallback(const int &delete_result,
                       int *result) {
   boost::mutex::scoped_lock lock(*mutex);
   *result = delete_result;
-  cond_var->notify_one();
+  cond_var->notify_all();
 };
 
 }  // namespace test_lsm
@@ -231,6 +231,7 @@ TEST_F(LocalStoreManagerTest, BEH_MAID_RemoveAllPacketsFromKey) {
         cond_var.wait(lock);
     }
     ASSERT_EQ(maidsafe::kSuccess, result);
+    result = maidsafe::kGeneralError;
   }
 
   // Remove said packets
