94308b10f5a548471a6fb99c10e53cb6c199385f
Fraser Hutchison <fraser.hutchison@maidsafe.net>
2010-04-20 13:23:26 +0100

    Added BreakPad ExceptionHandler to MSVC targets. Fixes to CMakeLists.

--- src/maidsafe/vault/win_service.cc
+++ src/maidsafe/vault/win_service.cc
@@ -28,9 +28,24 @@
 #include <boost/date_time/posix_time/posix_time.hpp>
 #include <string>
 #include "maidsafe/vault/vaultdaemon.h"
+#ifdef __MSVC__
+  #include <client/windows/handler/exception_handler.h>
+#endif
 
 #define LOGFILE "VaultService.txt"
 const int kSleepTime = 10000;  // milliseconds
+
+static bool DumpCallback(const wchar_t*,
+                         const wchar_t* minidump_id,
+                         void*,
+                         EXCEPTION_POINTERS*,
+                         MDRawAssertionInfo*,
+                         bool succeeded) {
+  if (succeeded) {
+    wprintf(L"%s is dumped.\n", minidump_id);
+  }
+  return succeeded;
+}
 
 int WriteToLog(char* str) {
   FILE* log;
@@ -60,6 +75,10 @@ void ControlHandler(DWORD request);
 int InitService();
 
 int main() {
+#ifdef __MSVC__
+  google_breakpad::ExceptionHandler eh(L".", NULL, DumpCallback,
+      NULL, google_breakpad::ExceptionHandler::HANDLER_ALL);
+#endif
   const size_t kMax(8);
   wchar_t service_name_[kMax];
   mbstowcs(service_name_, "PDVault", kMax);
