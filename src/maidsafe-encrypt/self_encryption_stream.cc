ea69f779c32f2e44688ff5744a630f89c7709f2e
Steve Muecklisch <steve.muecklisch@maidsafe.net>
2011-02-18 18:29:29 +0000

    Declared interface for SelfEncryptionStream, implementation still incomplete.

---
+++ src/maidsafe-encrypt/self_encryption_stream.cc
@@ -0,0 +1,99 @@
+/*******************************************************************************
+ *  Copyright 2011 maidsafe.net limited                                        *
+ *                                                                             *
+ *  The following source code is property of maidsafe.net limited and is not   *
+ *  meant for external use.  The use of this code is governed by the license   *
+ *  file LICENSE.TXT found in the root of this directory and also on           *
+ *  www.maidsafe.net.                                                          *
+ *                                                                             *
+ *  You are not free to copy, amend or otherwise use this source code without  *
+ *  the explicit written permission of the board of directors of maidsafe.net. *
+ ***************************************************************************//**
+ * @file  self_encryption_stream.cc
+ * @brief Provides self-en/decryption functionality through a stream interface.
+ * @date  2011-02-18
+ */
+
+#include "maidsafe-encrypt/self_encryption_stream.h"
+
+#include <iosfwd>
+#include <string>
+
+#include "maidsafe-encrypt/utils.h"
+#include "boost/filesystem/fstream.hpp"
+
+namespace fs = boost::filesystem3;
+namespace io = boost::iostreams;
+
+namespace maidsafe {
+
+namespace encrypt {
+
+SelfEncryptionDevice::SelfEncryptionDevice(const DataMap &data_map,
+                                           const fs::path &chunk_dir)
+    : data_map_(data_map),
+      chunk_dir_(chunk_dir),
+      total_size_(0),
+      offset_(0),
+      current_chunk_index_(0),
+      current_chunk_offset_(0),
+      current_chunk_content_() {
+  for (auto it = data_map_.chunks.begin(); it != data_map_.chunks.end(); ++it)
+    total_size_ += it->pre_size;
+}
+
+std::streamsize SelfEncryptionDevice::read(char* s, std::streamsize n) {
+  /* - determine which chunks the offset_ and offset_ + n locations translate to
+   * - iterate over all relevant chunks
+   *   - load chunk, if not already in memory
+   *   - self-decrypt content
+   *   - write to output buffer and increment pointer
+   * - update relevant member vars
+   */
+  return -1;
+}
+
+io::stream_offset SelfEncryptionDevice::seek(io::stream_offset offset,
+                                             std::ios_base::seekdir way) {
+  io::stream_offset new_offset;
+  switch (way) {
+    case std::ios_base::beg:
+      new_offset = offset;
+      break;
+    case std::ios_base::cur:
+      new_offset = offset_ + offset_;
+      break;
+    case std::ios_base::end:
+      new_offset = total_size_ + offset;
+      break;
+    default:
+      return -1;
+  }
+
+  if (new_offset < 0 || new_offset > total_size_)
+    return -1;
+
+  offset_ = new_offset;
+  return offset_;
+}
+
+bool SelfEncryptionDevice::LoadChunk(const fs::path &chunk_path,
+                                     std::string *content) {
+  try {
+    std::uintmax_t chunk_size(fs::file_size(chunk_path));
+    fs::ifstream chunk_in(chunk_path, fs::ifstream::binary);
+    if (!chunk_in.good())
+      return false;
+    content->resize(chunk_size);
+    chunk_in.read(&((*content)[0]), chunk_size);
+    chunk_in.close();
+  }
+  catch(...) {
+    return false;
+  }
+  return true;
+}
+
+}  // namespace encrypt
+
+}  // namespace maidsafe
---
+++ src/maidsafe-encrypt/self_encryption_stream.h
@@ -0,0 +1,71 @@
+/*******************************************************************************
+ *  Copyright 2011 maidsafe.net limited                                        *
+ *                                                                             *
+ *  The following source code is property of maidsafe.net limited and is not   *
+ *  meant for external use.  The use of this code is governed by the license   *
+ *  file LICENSE.TXT found in the root of this directory and also on           *
+ *  www.maidsafe.net.                                                          *
+ *                                                                             *
+ *  You are not free to copy, amend or otherwise use this source code without  *
+ *  the explicit written permission of the board of directors of maidsafe.net. *
+ ***************************************************************************//**
+ * @file  self_encryption_stream.h
+ * @brief Provides self-en/decryption functionality through a stream interface.
+ * @date  2011-02-18
+ */
+
+#ifndef MAIDSAFE_ENCRYPT_SELF_ENCRYPTION_STREAM_H_
+#define MAIDSAFE_ENCRYPT_SELF_ENCRYPTION_STREAM_H_
+
+#include <iosfwd>
+#include <string>
+
+#include "boost/filesystem.hpp"
+#include "boost/iostreams/concepts.hpp"
+#include "boost/iostreams/positioning.hpp"
+#include "boost/iostreams/stream.hpp"
+#include "maidsafe-encrypt/data_map.h"
+#include "maidsafe-encrypt/version.h"
+
+#if MAIDSAFE_ENCRYPT_VERSION < 2
+#error This API is not compatible with the installed library.\
+  Please update the maidsafe-encrypt library.
+#endif
+
+namespace fs = boost::filesystem3;
+namespace io = boost::iostreams;
+
+namespace boost { namespace iostreams {
+struct input_seekable_device_tag
+    : virtual device_tag, input_seekable, detail::one_head { };
+} }
+
+namespace maidsafe {
+
+namespace encrypt {
+
+class SelfEncryptionDevice : public io::device<io::input_seekable_device_tag> {
+ public:
+  SelfEncryptionDevice(const DataMap &data_map, const fs::path &chunk_dir);
+  virtual ~SelfEncryptionDevice() {}
+  std::streamsize read(char* s, std::streamsize n);
+  // std::streamsize write(const char_type* s, std::streamsize n);
+  io::stream_offset seek(io::stream_offset offset, std::ios_base::seekdir way);
+ private:
+  bool LoadChunk(const fs::path &chunk_path, std::string *content);
+  DataMap data_map_;
+  fs::path chunk_dir_;
+  std::streamsize total_size_;
+  io::stream_offset offset_;
+  size_t current_chunk_index_;
+  io::stream_offset current_chunk_offset_;
+  std::string current_chunk_content_;
+};
+
+typedef io::stream<SelfEncryptionDevice> SelfEncryptionStream;
+
+}  // namespace encrypt
+
+}  // namespace maidsafe
+
+#endif  // MAIDSAFE_ENCRYPT_SELF_ENCRYPTION_STREAM_H_
