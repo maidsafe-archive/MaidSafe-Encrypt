842a56c53c371253c945cd122a8a37f7ab2bad16
David Irvine <david.irvine@maidsafe.net>
2011-08-10 20:49:24 +0100

    changed dimensioned byte pointers for shared_array in tests.

--- src/maidsafe/encrypt/tests/test_utils.cc
+++ src/maidsafe/encrypt/tests/test_utils.cc
@@ -77,26 +77,27 @@ protected:
   SE selfenc_;// (chunk_store);
   size_t chunk_size_;// (1024*256); // system default
   size_t num_chunks_; //(10);
-  char * extra_content_; // = new char[5]{'1','2','3','4','5'};
+  boost::shared_array<char> extra_content_; // = new char[5]{'1','2','3','4','5'};
   size_t expected_content_size_; // ;
   size_t file_size_; // (chunk_size*num_chunks + expected_content_size);
-  byte *pre_enc_chunk_; // = new byte[chunk_size];
-  byte *pad_; // = new byte[144];
-  byte *xor_res_; // = new byte[chunk_size];
-  byte *prehash_; // = new byte[CryptoPP::SHA512::DIGESTSIZE];
-  byte *posthashxor_; // = new byte[CryptoPP::SHA512::DIGESTSIZE];
-  byte *postenc_; // = new byte[chunk_size];
-  byte *key_;// = new byte[32];
-  byte *iv_;// = new byte[16];
-  char *pre_enc_file_;// = new char[file_size];
+  boost::shared_array<byte> pre_enc_chunk_; // = new byte[chunk_size];
+  boost::shared_array<byte>pad_; // = new byte[144];
+  boost::shared_array<byte>xor_res_; // = new byte[chunk_size];
+  boost::shared_array<byte>prehash_; // = new byte[CryptoPP::SHA512::DIGESTSIZE];
+  boost::shared_array<byte>posthashxor_; // = new byte[CryptoPP::SHA512::DIGESTSIZE];
+  boost::shared_array<byte>postenc_; // = new byte[chunk_size];
+  boost::shared_array<byte>key_;// = new byte[32];
+  boost::shared_array<byte>iv_;// = new byte[16];
+  boost::shared_array<char>pre_enc_file_;// = new char[file_size];
 };
 
 TEST_F(SelfEncryptionTest, BEH_40Charsonly) {
   std::string content(RandomString(40));
-  char *stuff = new char[40];
-  char *answer = new char[40];
-  std::copy(content.c_str(), content.c_str() + 39, stuff); // null terminator
-  EXPECT_TRUE(selfenc_.Write(stuff, 40));
+  boost::shared_array<char>stuff(new char[40]);
+  boost::shared_array<char>answer(new char[40]);
+  std::copy(content.data(), content.data() + 40, stuff.get());
+  
+  EXPECT_TRUE(selfenc_.Write(stuff.get(), 40));
   EXPECT_EQ(0, selfenc_.getDataMap()->chunks.size());
   EXPECT_EQ(0, selfenc_.getDataMap()->size);
   EXPECT_EQ(0, selfenc_.getDataMap()->content_size);
@@ -104,21 +105,18 @@ TEST_F(SelfEncryptionTest, BEH_40Charsonly) {
   EXPECT_EQ(40, selfenc_.getDataMap()->size);
   EXPECT_EQ(40, selfenc_.getDataMap()->content_size);
   EXPECT_EQ(0, selfenc_.getDataMap()->chunks.size());
-  EXPECT_EQ(*stuff, *selfenc_.getDataMap()->content.c_str());
-  EXPECT_TRUE(selfenc_.Read(answer));
-  std::string sanswer(answer, 40);
-//   EXPECT_EQ(content,
-//             sanswer);
-  // FIXME (dirvine) do not use std::copy
+  EXPECT_EQ(*stuff.get(), *selfenc_.getDataMap()->content.c_str());
+  EXPECT_TRUE(selfenc_.Read(answer.get()));
+  EXPECT_EQ(*stuff.get(), *answer.get());
   EXPECT_TRUE(selfenc_.ReInitialise());
 }
 
 TEST_F(SelfEncryptionTest, BEH_1023Chars) {
   EXPECT_TRUE(selfenc_.ReInitialise());
   std::string content(RandomString(1023));
-  char *stuff1 = new char[1023];
-  std::copy(content.c_str(), content.c_str() + 1023, stuff1);
-  EXPECT_TRUE(selfenc_.Write(stuff1, 1023));
+  boost::shared_array<char>stuff1 (new char[1023]);
+  std::copy(content.c_str(), content.c_str() + 1023, stuff1.get());
+  EXPECT_TRUE(selfenc_.Write(stuff1.get(), 1023));
   EXPECT_EQ(0, selfenc_.getDataMap()->chunks.size());
   EXPECT_EQ(0, selfenc_.getDataMap()->size);
   EXPECT_EQ(0, selfenc_.getDataMap()->content_size);
@@ -131,9 +129,9 @@ TEST_F(SelfEncryptionTest, BEH_1023Chars) {
 TEST_F(SelfEncryptionTest, BEH_1025Chars3chunks) {
   EXPECT_TRUE(selfenc_.ReInitialise());
   std::string content(RandomString(1025));
-  char *stuff1 = new char[1025];
-  std::copy(content.c_str(), content.c_str() + 1025, stuff1);
-  EXPECT_TRUE(selfenc_.Write(stuff1, 1025));
+  boost::shared_array<char>stuff1 (new char[1025]);
+  std::copy(content.c_str(), content.c_str() + 1025, stuff1.get());
+  EXPECT_TRUE(selfenc_.Write(stuff1.get(), 1025));
   EXPECT_EQ(0, selfenc_.getDataMap()->chunks.size());
   EXPECT_EQ(0, selfenc_.getDataMap()->size);
   EXPECT_EQ(0, selfenc_.getDataMap()->content_size);
@@ -155,7 +153,7 @@ TEST_F(SelfEncryptionTest, BEH_WriteAndRead) {
   EXPECT_TRUE(selfenc_.ReInitialise());
   size_t test_data_size(1024*1024*10); // less than 2 mB fails due to test
   std::string plain_text(RandomString(test_data_size));
-  char *plain_data = new char[test_data_size];
+  boost::shared_array<char>plain_data (new char[test_data_size]);
   for (size_t i = 0; i < test_data_size ; ++i) {
     plain_data[i] = /*'a'; //*/plain_text[i];
   }
@@ -165,7 +163,7 @@ TEST_F(SelfEncryptionTest, BEH_WriteAndRead) {
  
   boost::posix_time::ptime time =
         boost::posix_time::microsec_clock::universal_time();
-  ASSERT_TRUE(selfenc_.Write(plain_data, test_data_size));
+  ASSERT_TRUE(selfenc_.Write(plain_data.get(), test_data_size));
   ASSERT_TRUE(selfenc_.FinaliseWrite());
   std::uint64_t duration =
       (boost::posix_time::microsec_clock::universal_time() -
@@ -177,9 +175,9 @@ TEST_F(SelfEncryptionTest, BEH_WriteAndRead) {
              << " seconds at a speed of "
              <<  BytesToBinarySiUnits(test_data_size / (duration / 1000000.0) )
              << "/s" << std::endl;
-  char * answer = new char[test_data_size];
+  boost::shared_array<char>answer (new char[test_data_size]);
   time =  boost::posix_time::microsec_clock::universal_time();
-  ASSERT_TRUE(selfenc_.Read(answer, test_data_size, 0));
+  ASSERT_TRUE(selfenc_.Read(answer.get(), test_data_size, 0));
   duration = (boost::posix_time::microsec_clock::universal_time() -
               time).total_microseconds();
   if (duration == 0)
@@ -191,7 +189,7 @@ TEST_F(SelfEncryptionTest, BEH_WriteAndRead) {
              << "/s" << std::endl;
 
   for (size_t  i = 0; i < test_data_size ; ++i)
-    ASSERT_EQ(plain_data[i], answer[i]) << "failed at count " << i;
+    ASSERT_EQ(plain_data.get()[i], answer.get()[i]) << "failed at count " << i;
 
 }
 
@@ -199,7 +197,7 @@ TEST_F(SelfEncryptionTest, BEH_WriteAndReadByteAtATime) {
   EXPECT_TRUE(selfenc_.ReInitialise());
   size_t test_data_size(1024*1024*10); // less than 2 mB fails due to test
   std::string plain_text(RandomString(test_data_size));
-  char *plain_data = new char[test_data_size];
+  boost::shared_array<char>plain_data (new char[test_data_size]);
   for (size_t i = 0; i < test_data_size ; ++i) {
     plain_data[i] = plain_text[i];
   }
@@ -208,7 +206,7 @@ TEST_F(SelfEncryptionTest, BEH_WriteAndReadByteAtATime) {
   boost::posix_time::ptime time =
         boost::posix_time::microsec_clock::universal_time();
   for (size_t i = 0; i < test_data_size ; ++i)  {
-    selfenc_.Write(plain_data, test_data_size);
+    selfenc_.Write(plain_data.get(), test_data_size);
   }
   ASSERT_TRUE(selfenc_.FinaliseWrite());
   std::uint64_t duration =
@@ -221,9 +219,9 @@ TEST_F(SelfEncryptionTest, BEH_WriteAndReadByteAtATime) {
              << " seconds at a speed of "
              <<  BytesToBinarySiUnits(test_data_size / (duration / 1000000.0) )
              << "/s at Byte per time" << std::endl;
-  char * answer = new char[test_data_size];
+  boost::shared_array<char>answer (new char[test_data_size]);
   time =  boost::posix_time::microsec_clock::universal_time();
-  ASSERT_TRUE(selfenc_.Read(answer, test_data_size, 0));
+  ASSERT_TRUE(selfenc_.Read(answer.get(), test_data_size, 0));
   duration = (boost::posix_time::microsec_clock::universal_time() -
               time).total_microseconds();
   if (duration == 0)
@@ -235,7 +233,7 @@ TEST_F(SelfEncryptionTest, BEH_WriteAndReadByteAtATime) {
              << "/s" << std::endl;
 
   for (size_t  i = 0; i < test_data_size ; ++i)
-    ASSERT_EQ(plain_data[i], answer[i]) << "failed at count " << i;
+    ASSERT_EQ(plain_data.get()[i], answer.get()[i]) << "failed at count " << i;
 }
 
 
@@ -244,7 +242,7 @@ TEST_F(SelfEncryptionTest, BEH_WriteAndReadByteAtATimeOutOfSequenceForward) {
   EXPECT_TRUE(selfenc_.ReInitialise());
   size_t test_data_size(1024*1024*10); // less than 2 mB fails due to test
   std::string plain_text(RandomString(test_data_size));
-  char *plain_data = new char[test_data_size];
+  boost::shared_array<char>plain_data  (new char[test_data_size]);
   for (size_t i = 0; i < test_data_size ; ++i) {
     plain_data[i] = plain_text[i];
   }
@@ -254,10 +252,10 @@ TEST_F(SelfEncryptionTest, BEH_WriteAndReadByteAtATimeOutOfSequenceForward) {
         boost::posix_time::microsec_clock::universal_time();
 
   for (size_t i = 0; i < test_data_size ; i += 2)  {
-    ASSERT_TRUE(selfenc_.Write(plain_data, test_data_size));
+    ASSERT_TRUE(selfenc_.Write(plain_data.get(), test_data_size));
   }
   for (size_t i = 1; i < test_data_size ; i +=2 )  {
-    ASSERT_TRUE(selfenc_.Write(plain_data, test_data_size));
+    ASSERT_TRUE(selfenc_.Write(plain_data.get(), test_data_size));
   }
   ASSERT_TRUE(selfenc_.FinaliseWrite());
   std::uint64_t duration =
@@ -270,9 +268,9 @@ TEST_F(SelfEncryptionTest, BEH_WriteAndReadByteAtATimeOutOfSequenceForward) {
              << " seconds at a speed of "
              <<  BytesToBinarySiUnits(test_data_size / (duration / 1000000.0) )
              << "/s at Byte per time" << std::endl;
-  char * answer = new char[test_data_size];
+  boost::shared_array<char>answer (new char[test_data_size]);
   time =  boost::posix_time::microsec_clock::universal_time();
-  ASSERT_TRUE(selfenc_.Read(answer, test_data_size, 0));
+  ASSERT_TRUE(selfenc_.Read(answer.get(), test_data_size, 0));
   duration = (boost::posix_time::microsec_clock::universal_time() -
               time).total_microseconds();
   if (duration == 0)
@@ -284,7 +282,7 @@ TEST_F(SelfEncryptionTest, BEH_WriteAndReadByteAtATimeOutOfSequenceForward) {
              << "/s" << std::endl;
 
   for (size_t  i = 0; i < test_data_size ; ++i)
-    ASSERT_EQ(plain_data[i], answer[i]) << "failed at count " << i;
+    ASSERT_EQ(plain_data.get()[i], answer.get()[i]) << "failed at count " << i;
 }
 
 
@@ -300,18 +298,18 @@ TEST_F(SelfEncryptionTest, BEH_manual_check_write) {
   SE selfenc(chunk_store, data_map);
   size_t chunk_size(1024*256); // system default
   size_t num_chunks(10);
-  char * extra_content = new char[5]{'1','2','3','4','5'};
+  boost::shared_array<char>extra_content(new char[5]{'1','2','3','4','5'});
   size_t expected_content_size(sizeof(extra_content));
   size_t file_size(chunk_size*num_chunks + expected_content_size);
-  byte *pre_enc_chunk = new byte[chunk_size];
-  byte *pad = new byte[144];
-  byte *xor_res = new byte[chunk_size];
-  byte *prehash = new byte[CryptoPP::SHA512::DIGESTSIZE];
-  byte *posthashxor = new byte[CryptoPP::SHA512::DIGESTSIZE];
-  byte *postenc = new byte[chunk_size];
-  byte *key = new byte[32];
-  byte *iv = new byte[16];
-  char *pre_enc_file = new char[file_size];
+  boost::shared_array<byte> pre_enc_chunk(new byte[chunk_size]);
+  boost::shared_array<byte>pad(new byte[144]);
+  boost::shared_array<byte>xor_res(new byte[chunk_size]);
+  boost::shared_array<byte>prehash(new byte[CryptoPP::SHA512::DIGESTSIZE]);
+  boost::shared_array<byte>posthashxor(new byte[CryptoPP::SHA512::DIGESTSIZE]);
+  boost::shared_array<byte>postenc(new byte[chunk_size]);
+  boost::shared_array<byte>key(new byte[32]);
+  boost::shared_array<byte>iv(new byte[16]);
+  boost::shared_array<char>pre_enc_file(new char[file_size]);
 
   for (size_t i = 0; i < chunk_size; ++i) {
     pre_enc_chunk[i] = 'b';
@@ -322,14 +320,15 @@ TEST_F(SelfEncryptionTest, BEH_manual_check_write) {
   }
 
   EXPECT_TRUE(selfenc.ReInitialise());
-  EXPECT_TRUE(selfenc.Write(pre_enc_file, file_size));
+  EXPECT_TRUE(selfenc.Write(pre_enc_file.get(), file_size));
   EXPECT_TRUE(selfenc.FinaliseWrite());
 // Do some testing on results
   EXPECT_EQ(num_chunks,  selfenc.getDataMap()->chunks.size());
   EXPECT_EQ(expected_content_size,  selfenc.getDataMap()->content_size);
   EXPECT_EQ(file_size, selfenc.getDataMap()->size);
 
-  CryptoPP::SHA512().CalculateDigest(prehash, pre_enc_chunk, chunk_size);
+  CryptoPP::SHA512().CalculateDigest(prehash.get(), pre_enc_chunk.get(),
+                                     chunk_size);
 
   for (int i = 0; i < 64; ++i) {
     pad[i] = prehash[i];
@@ -338,18 +337,19 @@ TEST_F(SelfEncryptionTest, BEH_manual_check_write) {
   for (int i = 0; i < 16; ++i) {
     pad[i+128] = prehash[i+48];
   }
-  std::copy(prehash, prehash + 32, key);
-  std::copy(prehash + 32, prehash + 48, iv);
+  
+  std::copy(prehash.get(), prehash.get() + 32, key.get());
+  std::copy(prehash.get() + 32, prehash.get() + 48, iv.get());
 
-  CryptoPP::CFB_Mode<CryptoPP::AES>::Encryption enc(key, 32, iv);
-  enc.ProcessData(postenc, pre_enc_chunk, chunk_size);
+  CryptoPP::CFB_Mode<CryptoPP::AES>::Encryption enc(key.get(), 32, iv.get());
+  enc.ProcessData(postenc.get(), pre_enc_chunk.get(), chunk_size);
 
 
   for (size_t i = 0; i < chunk_size; ++i) {
     xor_res[i] = postenc[i]^pad[i%144];
   }
 
-  CryptoPP::SHA512().CalculateDigest(posthashxor, xor_res, chunk_size);
+  CryptoPP::SHA512().CalculateDigest(posthashxor.get(), xor_res.get(), chunk_size);
   
   for (int i = 0; i < 64; ++i) {
     ASSERT_EQ(prehash[i], selfenc.getDataMap()->chunks[4].pre_hash[i])
