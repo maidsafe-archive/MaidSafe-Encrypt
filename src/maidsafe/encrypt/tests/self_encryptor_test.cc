3e1a972ad0f69ed7817379ca6cf181c7b7b1a2dd
Brian Smith <brian.smith@maidsafe.net>
2012-11-27 17:30:57 +0000

    Finishes [#40108357]

--- src/maidsafe/encrypt/tests/self_encryptor_test.cc
+++ src/maidsafe/encrypt/tests/self_encryptor_test.cc
@@ -1177,7 +1177,7 @@ TEST_F(BasicTest, BEH_DeleteStoredChunkFromDisk) {
   EXPECT_TRUE(self_encryptor_->Write(original.data(), static_cast<uint32_t>(original.size()), 0));
   EXPECT_TRUE(self_encryptor_->Flush());
   fs::directory_iterator it(file_chunk_store_path_);
-  EXPECT_NE(0, fs::remove_all(it->path(), error_code));
+  EXPECT_EQ(6, fs::remove_all(it->path(), error_code)); // directory depth == 5
   EXPECT_EQ(0, error_code.value());
   EXPECT_FALSE(self_encryptor_->Read(const_cast<char*>(recovered.data()), recovered.size(), 0));
 }
