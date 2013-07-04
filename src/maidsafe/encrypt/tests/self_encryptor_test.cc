4bf2140df452a827544c0499c703660bc9f1aa08
Brian Smith <brian.smith@maidsafe.net>
2013-07-04 13:49:30 +0100

    Variable name change and test fix.

--- src/maidsafe/encrypt/tests/self_encryptor_test.cc
+++ src/maidsafe/encrypt/tests/self_encryptor_test.cc
@@ -1131,20 +1131,41 @@ TEST_F(BasicTest, BEH_DeleteStoredChunkFromDisk) {
   boost::system::error_code error_code;
   uint32_t size = 5 * 256 * 1024 + 3;
   std::string original(RandomString(size)), recovered(size, 0);
-  EXPECT_TRUE(self_encryptor_->Write(original.data(), static_cast<uint32_t>(original.size()), 0));
-  EXPECT_TRUE(self_encryptor_->Flush());
-  EXPECT_TRUE(self_encryptor_->Read(const_cast<char*>(recovered.data()),
-                                    static_cast<uint32_t>(recovered.size()),
-                                    0));
-  fs::directory_iterator it(file_chunk_store_path_);
+
+  {
+    EXPECT_TRUE(self_encryptor_->Write(original.data(), static_cast<uint32_t>(original.size()), 0));
+    EXPECT_TRUE(self_encryptor_->Flush());
+    EXPECT_TRUE(self_encryptor_->Read(const_cast<char*>(recovered.data()),
+                                      static_cast<uint32_t>(recovered.size()),
+                                      0));
+  }
+
+  {
+    SelfEncryptorPtr self_encryptor(new SelfEncryptor(data_map_,
+                                                      *client_nfs_,
+                                                      *data_store_,
+                                                      num_procs_));
+    EXPECT_TRUE(self_encryptor->Read(const_cast<char*>(recovered.data()),
+                                     static_cast<uint32_t>(recovered.size()),
+                                     0));
+  }
+
+  fs::directory_iterator it(data_store_path_);
   while (fs::is_regular_file(it->path()))
     ++it;
   EXPECT_FALSE(it == fs::directory_iterator());
   EXPECT_GE(fs::remove_all(it->path(), error_code), 6);  // for directory depth == 5
   EXPECT_EQ(0, error_code.value());
-  EXPECT_FALSE(self_encryptor_->Read(const_cast<char*>(recovered.data()),
-                                     static_cast<uint32_t>(recovered.size()),
-                                     0));
+
+  {
+    SelfEncryptorPtr self_encryptor(new SelfEncryptor(data_map_,
+                                                      *client_nfs_,
+                                                      *data_store_,
+                                                      num_procs_));
+    EXPECT_FALSE(self_encryptor->Read(const_cast<char*>(recovered.data()),
+                                      static_cast<uint32_t>(recovered.size()),
+                                      0));
+  }
 }
 
 TEST_F(BasicTest, BEH_ManualCheckWrite) {
